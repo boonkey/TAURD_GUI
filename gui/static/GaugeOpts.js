10ba409e12119a21ec5bead02ac6b5282407daff
BelgishChoko <guyazran@gmail.com>
2018-03-17 17:19:59 +0200

    Alpha Design

---
+++ gui/static/GaugeOpts.js
@@ -0,0 +1,215 @@
+
+/**
+ * ---------------------
+ *     Gauge Options
+ * ---------------------
+ */
+
+
+var SpeedGaugeOptions = {
+    angle: -0.1, // The span of the speedGauge arc
+    lineWidth: 0.44, // The line thickness
+    radiusScale: 0.8, // Relative radius
+    pointer: {
+        length: 0.6, // // Relative to speedGauge radius
+        strokeWidth: 0.035, // The thickness
+        color: '#EEEEEE' // Fill color
+    },
+
+    staticLabels: {
+        font: "14px sans-serif",  // Specifies font
+        labels: [0, 20, 40, 60, 80, 100, 120],  // Print labels at these values
+        color: "#000000",  // Optional: Label text color
+        fractionDigits: 0  // Optional: Numerical precision. 0=round off.
+    },
+
+    limitMax: true,     // If false, max value increases automatically if value > maxValue
+    limitMin: true,     // If true, the min value of the speedGauge will be fixed
+
+
+    // generateGradient: true,
+    highDpiSupport: true,     // High resolution support
+
+    staticZones: [
+        {strokeStyle: "#333333", min: 0, max: 120, height: 1.0},  // Red
+        // {strokeStyle: "#F03E3E", min: 100, max: 120, height: 1.5}  // Red
+    ],
+
+    renderTicks: {
+        divisions: 6,
+        divWidth: 1.1,
+        divLength: 0.4,
+        divColor: "#cccccc",
+        subDivisions: 4,
+        subLength: 0.2,
+        subWidth: 0.6,
+        subColor: "#cccccc"
+    }
+};
+
+var rpmGaugeOptions = {
+    angle: -0.1, // The span of the speedGauge arc
+    lineWidth: 0.44, // The line thickness
+    radiusScale: 0.8, // Relative radius
+    pointer: {
+        length: 0.6, // // Relative to speedGauge radius
+        strokeWidth: 0.035, // The thickness
+        color: '#EEEEEE' // Fill color
+    },
+
+    staticLabels: {
+        font: "14px sans-serif",  // Specifies font
+        labels: [0, 1000, 2000, 3000, 4000, 5000, 6000, 7000, 8000],  // Print labels at these
+        // values
+        color: "#000000",  // Optional: Label text color
+        fractionDigits: 0  // Optional: Numerical precision. 0=round off.
+    },
+
+    limitMax: true,     // If false, max value increases automatically if value > maxValue
+    limitMin: true,     // If true, the min value of the speedGauge will be fixed
+
+
+    // generateGradient: true,
+    highDpiSupport: true,     // High resolution support
+
+    staticZones: [
+        {strokeStyle: "#333333", min: 0, max: 6500, height: 1.5},  // Black
+        {strokeStyle: "#A08800", min: 6500, max: 7000, height: 1.5},  // Yellow
+        {strokeStyle: "#F03E3E", min: 7000, max: 8000, height: 1.5}  // Red
+    ],
+
+    renderTicks: {
+        divisions: 8,
+        divWidth: 1.1,
+        divLength: 0.4,
+        divColor: "#cccccc",
+        subDivisions: 2 ,
+        subLength: 0.2,
+        subWidth: 0.6,
+        subColor: "#cccccc"
+    }
+};
+
+var breakingGaugeOptions = {
+    angle: -0.1, // The span of the speedGauge arc
+    lineWidth: 0.44, // The line thickness
+    radiusScale: 0.8, // Relative radius
+    pointer: {
+        length: 0.6, // // Relative to speedGauge radius
+        strokeWidth: 0.035, // The thickness
+        color: '#EEEEEE' // Fill color
+    },
+
+    staticLabels: {
+        font: "14px sans-serif",  // Specifies font
+        labels: [0, 10, 20, 30, 40, 50, 60, 70, 80, 90, 100],  // Print labels at these values
+        color: "#000000",  // Optional: Label text color
+        fractionDigits: 0  // Optional: Numerical precision. 0=round off.
+    },
+
+    limitMax: true,     // If false, max value increases automatically if value > maxValue
+    limitMin: true,     // If true, the min value of the speedGauge will be fixed
+
+
+    // generateGradient: true,
+    highDpiSupport: true,     // High resolution support
+
+    staticZones: [
+        {strokeStyle: "#333333", min: 0, max: 100, height: 1.5},  // Red
+        // {strokeStyle: "#F03E3E", min: 200, max: 220, height: 1.5}  // Red
+    ],
+
+    renderTicks: {
+        divisions: 10,
+        divWidth: 1.1,
+        divLength: 0.4,
+        divColor: "#cccccc",
+        subDivisions: 2,
+        subLength: 0.2,
+        subWidth: 0.6,
+        subColor: "#cccccc"
+    }
+};
+
+var throttleGaugeOptions = {
+    angle: -0.1, // The span of the speedGauge arc
+    lineWidth: 0.44, // The line thickness
+    radiusScale: 0.8, // Relative radius
+    pointer: {
+        length: 0.6, // // Relative to speedGauge radius
+        strokeWidth: 0.035, // The thickness
+        color: '#EEEEEE' // Fill color
+    },
+
+    staticLabels: {
+        font: "14px sans-serif",  // Specifies font
+        labels: [0, 10, 20, 30, 40, 50, 60, 70, 80, 90, 100],  // Print labels at these values
+        color: "#000000",  // Optional: Label text color
+        fractionDigits: 0  // Optional: Numerical precision. 0=round off.
+    },
+
+    limitMax: true,     // If false, max value increases automatically if value > maxValue
+    limitMin: true,     // If true, the min value of the speedGauge will be fixed
+
+
+    // generateGradient: true,
+    highDpiSupport: true,     // High resolution support
+
+    staticZones: [
+        {strokeStyle: "#333333", min: 0, max: 100, height: 1.5},  // Red
+        // {strokeStyle: "#F03E3E", min: 200, max: 220, height: 1.5}  // Red
+    ],
+
+    renderTicks: {
+        divisions: 10,
+        divWidth: 1.1,
+        divLength: 0.4,
+        divColor: "#cccccc",
+        subDivisions: 2,
+        subLength: 0.2,
+        subWidth: 0.6,
+        subColor: "#cccccc"
+    }
+};
+
+var wingPosGaugeOptions = {
+    angle: -0.2, // The span of the speedGauge arc
+    lineWidth: 0.44, // The line thickness
+    radiusScale: 0.8, // Relative radius
+    pointer: {
+        length: 0.6, // // Relative to speedGauge radius
+        strokeWidth: 0.035, // The thickness
+        color: '#EEEEEE' // Fill color
+    },
+
+    staticLabels: {
+        font: "14px sans-serif",  // Specifies font
+        labels: [-100, -80, -60, -40, -20, 0, 20, 40, 60, 80, 100],  // Print labels at these values
+        color: "#000000",  // Optional: Label text color
+        fractionDigits: 0  // Optional: Numerical precision. 0=round off.
+    },
+
+    limitMax: true,     // If false, max value increases automatically if value > maxValue
+    limitMin: true,     // If true, the min value of the speedGauge will be fixed
+
+
+    // generateGradient: true,
+    highDpiSupport: true,     // High resolution support
+
+    staticZones: [
+        {strokeStyle: "#F03E3E", min: -100, max: -90, height: 0.7},  // Red
+        {strokeStyle: "#333333", min: -90, max: 90, height: 0.7},  // Red
+        {strokeStyle: "#F03E3E", min: 90, max: 100, height: 0.7}  // Red
+    ],
+
+    renderTicks: {
+        divisions: 10,
+        divWidth: 1.1,
+        divLength: 0.4,
+        divColor: "#cccccc",
+        subDivisions: 4,
+        subLength: 0.2,
+        subWidth: 0.6,
+        subColor: "#cccccc"
+    }
+};
--- gui/static/Main.js
+++ gui/static/Main.js
@@ -1,98 +1,98 @@
 var isBrowserReady = false;
 
-var textBox;
-var gauge;
+var speedGauge;
+var rpmGauge;
+var breakingGauge;
+var throttleGauge;
+var wingPosGauge;
+
+
 var $speedLabel;
-
-var exports = {}
-
-var require = function(path) {
-    return exports;
-}
-
-
-/**
- * ---------------------
- *     Gauge Options    
- * ---------------------
- */
-
-
-var SpeedGaugeOptions = {
-    angle: -0.1, // The span of the gauge arc
-    lineWidth: 0.44, // The line thickness
-    radiusScale: 0.8, // Relative radius
-    pointer: {
-        length: 0.6, // // Relative to gauge radius
-        strokeWidth: 0.035, // The thickness
-        color: '#EEEEEE' // Fill color
-    },
-
-    staticLabels: {
-        font: "14px sans-serif",  // Specifies font
-        labels: [0, 20, 40, 60, 80, 100, 120, 140, 160, 180, 200, 220],  // Print labels at these values
-        color: "#000000",  // Optional: Label text color
-        fractionDigits: 0  // Optional: Numerical precision. 0=round off.
-    },
-
-    limitMax: true,     // If false, max value increases automatically if value > maxValue
-    limitMin: true,     // If true, the min value of the gauge will be fixed
-    
-
-    // generateGradient: true,
-    highDpiSupport: true,     // High resolution support
-
-    staticZones: [
-        {strokeStyle: "#333333", min: 0, max: 200, height: 1.5},  // Red
-        {strokeStyle: "#F03E3E", min: 200, max: 220, height: 1.5}  // Red
-     ],
-
-    renderTicks: {
-        divisions: 11,
-        divWidth: 1.1,
-        divLength: 0.7,
-        divColor: "#cccccc",
-        subDivisions: 4,
-        subLength: 0.5,
-        subWidth: 0.6,
-        subColor: "#cccccc"
-      }
-}
+var $rpmLabel;
+var $breakingLabel;
+var $throttleLabel;
+var $wingPosLabel;
 
 
 /**
  * Handle UI elements when doc is ready
  */
 $(document).ready(function(){
-    isBrowserReady = true
-    
-    var target = document.getElementById('testGauge'); // your canvas element
-    gauge = new Gauge(target).setOptions(SpeedGaugeOptions); // create sexy gauge!
-    
-    gauge.maxValue = 220; // set max gauge value
-    gauge.setMinValue(0);  // Prefer setter over gauge.minValue = 0
-    gauge.animationSpeed = 3;
-    gauge.set(0); // set actual value
 
-    $speedLabel = $('#textField');
 
-    // gauge.setTextField(document.getElementById("textField"));
+    // speed
+    var gauge = $("#speedGauge");
 
-    textBox = document.getElementById('value');
+    var target = gauge.find(".gauge")[0]; // your canvas element
+    speedGauge = new Gauge(target).setOptions(SpeedGaugeOptions); // create sexy speedGauge!
+    speedGauge.maxValue = 120; // set max speedGauge value
+    speedGauge.setMinValue(0);  // Prefer setter over speedGauge.minValue = 0
+    speedGauge.animationSpeed = 3;
+    speedGauge.set(0); // set actual value
+    $speedLabel = gauge.find('.gaugeTitle')[0];
 
+    // rpm
+    gauge = $("#rpmGauge");
+
+    target = gauge.find(".gauge")[0]; // your canvas element
+    rpmGauge = new Gauge(target).setOptions(rpmGaugeOptions); // create sexy speedGauge!
+    rpmGauge.maxValue = 8000; // set max speedGauge value
+    rpmGauge.setMinValue(0);  // Prefer setter over speedGauge.minValue = 0
+    rpmGauge.animationSpeed = 3;
+    rpmGauge.set(1); // set actual value
+    $rpmLabel = gauge.find('.gaugeTitle')[0];
+
+    // breaking
+    gauge = $("#breakingGauge");
+
+    target = gauge.find(".gauge")[0]; // your canvas element
+    breakingGauge = new Gauge(target).setOptions(breakingGaugeOptions); // create sexy speedGauge!
+    breakingGauge.maxValue = 100; // set max speedGauge value
+    breakingGauge.setMinValue(0);  // Prefer setter over speedGauge.minValue = 0
+    breakingGauge.animationSpeed = 3;
+    breakingGauge.set(0); // set actual value
+    $breakingLabel = gauge.find('.gaugeTitle')[0];
+
+
+    // throttle
+    gauge = $("#throttleGauge");
+
+    target = gauge.find(".gauge")[0]; // your canvas element
+    throttleGauge = new Gauge(target).setOptions(throttleGaugeOptions); // create sexy speedGauge!
+    throttleGauge.maxValue = 100; // set max speedGauge value
+    throttleGauge.setMinValue(0);  // Prefer setter over speedGauge.minValue = 0
+    throttleGauge.animationSpeed = 3;
+    throttleGauge.set(0); // set actual value
+    $throttleLabel = gauge.find('.gaugeTitle')[0];
+
+    // wingPos
+    gauge = $("#wingPosGauge");
+
+    target = gauge.find(".gauge")[0]; // your canvas element
+    wingPosGauge = new Gauge(target).setOptions(wingPosGaugeOptions); // create sexy speedGauge!
+    wingPosGauge.maxValue = 100; // set max speedGauge value
+    wingPosGauge.setMinValue(-100);  // Prefer setter over speedGauge.minValue = 0
+    wingPosGauge.animationSpeed = 3;
+    wingPosGauge.set(0); // set actual value
+    $wingPosLabel = gauge.find('.gaugeTitle')[0];
 });
 
+updateGauge = function(gaugeData) {
+    speedGauge.set(parseFloat(gaugeData.speed));
+    $speedLabel.text(gaugeData.speed);
 
-function buttonClicked(){
-    updateGauge(parseFloat(textBox.value, 10));
-}
+    rpmGauge.set(parseFloat(gaugeData.rpm));
+    $rpmLabel.text(gaugeData.rpm);
 
-updateGauge = function(newValue) {
-    if (isBrowserReady) {
-        gauge.set(parseFloat(newValue));
-        $speedLabel.text(newValue);
-    }
-}
+    breakingGauge.set(parseFloat(gaugeData.breaking));
+    $breakingLabel.text(gaugeData.breaking);
+
+    throttleGauge.set(parseFloat(gaugeData.throttle));
+    $throttleLabel.text(gaugeData.throttle);
+
+    wingPosGauge.set(parseFloat(gaugeData.wing_position));
+    $wingPosLabel.text(gaugeData.wing_position);
+};
 
 /**
  * get request
@@ -100,10 +100,9 @@ updateGauge = function(newValue) {
 function getGaugeUpdate(){
     $.ajax({
         type: 'GET',
-        url: 'http://localhost:8080/?dan=shakdan',
+        url: 'http://localhost:8080/?task=0',
         success: function(data) {
-            console.log(data.speed);
-            updateGauge(data.speed)
+            updateGauge(data)
         },
         error: function(){
             alert("error loading user count");
